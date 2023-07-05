9c329de947a1bf462d6ba64522e3a43e2a96bb5a
kavyam18 <101625469+kavyam18@users.noreply.github.com>
2023-07-05 22:45:16 +0530

    Add files via upload

---
+++ Bubble/script.js
@@ -0,0 +1,16 @@
+function circle1(){
+    document.getElementById("arrow1").style.cssText="position:absolute;top:85px;left:122px"
+    document.getElementById("circle1").style.cssText="background-color:grey"
+  }
+  function circle2(){
+    document.getElementById("arrow2").style.cssText="position:absolute;top:200px;left:122px"
+    document.getElementById("circle2").style.cssText="background-color:grey"
+  }
+  function circle3(){
+    document.getElementById("arrow3").style.cssText="position:absolute;top:310px;left:122px"
+    document.getElementById("circle3").style.cssText="background-color:grey"
+  }
+  function circle4(){
+    document.getElementById("arrow4").style.cssText="position:absolute;top:420px;left:122px"
+    document.getElementById("circle4").style.cssText="background-color:grey"
+  }
